8214eac0ecf31f4181bd3dca132f994c3e86895f
greewank <96386149+greewank@users.noreply.github.com>
2022-01-13 12:55:07 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const colors = ["green", "blue", "rgba(133,122,200)", 
+"#f15025", "rgba(120, 157, 220, 0.5)"];
+
+const button = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+button.addEventListener('click', function(){
+
+    // The goal is to get a random number between 0 and 3.
+    const randomNumber = getRandomNumber();
+    // console.log(document.body);
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length);
+}
---
+++ hex.js
@@ -0,0 +1,21 @@
+ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+// An example of a hex color: #f15025
+ const btn = document.getElementById('btn');
+ const color = document.querySelector('.color');
+
+ btn.addEventListener('click', function(){
+    let hexColor = '#';
+    const randomNumber = getRandomNumber();
+    for(let i = 0; i<6; i++){
+        hexColor += hex[getRandomNumber()];
+    }
+
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+ });
+
+//  Textcontent shows the textcontent of all elements in the node and its descendents. 
+ function getRandomNumber(){
+     return Math.floor(Math.random() * hex.length);
+ }
